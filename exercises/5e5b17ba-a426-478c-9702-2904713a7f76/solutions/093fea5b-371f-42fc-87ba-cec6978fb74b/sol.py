e3bcad287508146060fe380a59139c8dee4a3ef8
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-24 19:19:34 +0100

    created file exercises/5e5b17ba-a426-478c-9702-2904713a7f76/solutions/093fea5b-371f-42fc-87ba-cec6978fb74b/sol.py

---
+++ exercises/5e5b17ba-a426-478c-9702-2904713a7f76/solutions/093fea5b-371f-42fc-87ba-cec6978fb74b/sol.py
@@ -0,0 +1,12 @@
+marke = 'Citroen'
+modelis = 'Xsara'
+metai = 2002
+rida = 148974
+
+sakinys = f"""
+{marke} {modelis} 
+buvo pagaminta {metai} metais
+ir nuvažiavo {rida} km.
+"""
+
+print(sakinys)
