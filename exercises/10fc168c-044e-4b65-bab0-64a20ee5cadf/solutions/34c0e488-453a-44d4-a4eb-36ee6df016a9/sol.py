b6a7c6da7892d9a90ac14dad7febc0772bdd84af
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-24 20:07:59 +0100

    created file exercises/10fc168c-044e-4b65-bab0-64a20ee5cadf/solutions/34c0e488-453a-44d4-a4eb-36ee6df016a9/sol.py

---
+++ exercises/10fc168c-044e-4b65-bab0-64a20ee5cadf/solutions/34c0e488-453a-44d4-a4eb-36ee6df016a9/sol.py
@@ -0,0 +1,9 @@
+def stars(num=15):
+	return '*'*num
+
+# ---------------------------------- #
+# Testuoti stars(). NELIESKITE.      #
+# ---------------------------------- #
+try: print(stars(int(input())))      #
+except: print(stars())               #
+# ---------------------------------- #
