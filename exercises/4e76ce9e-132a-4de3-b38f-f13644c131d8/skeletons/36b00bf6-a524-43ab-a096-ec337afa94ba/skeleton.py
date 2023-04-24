96c30ec3b7bdf6b4f93f4eb536ce6a5af7546d0c
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-24 20:09:05 +0100

    created file exercises/4e76ce9e-132a-4de3-b38f-f13644c131d8/skeletons/36b00bf6-a524-43ab-a096-ec337afa94ba/skeleton.py

---
+++ exercises/4e76ce9e-132a-4de3-b38f-f13644c131d8/skeletons/36b00bf6-a524-43ab-a096-ec337afa94ba/skeleton.py
@@ -0,0 +1,24 @@
+# Pavyzdys
+def du_rezultatai(x):
+    return int(x), x%1
+
+
+# Jūsų funkcija kvadratine_lygtis()
+def kvadratine_lygtis():
+
+    pass
+
+
+
+
+
+# ------------------------------------ #
+#     Nekeiskite šio kodo              | 
+# ------------------------------------ #
+try:
+    print(kvadratine_lygtis(*map(int, input().split())))  
+except:
+    print(kvadratine_lygtis(-1,2,3))                  
+    print(kvadratine_lygtis(1,2,1))                   
+    print(kvadratine_lygtis(2,1,1))                   
+# ------------------------------------ #
