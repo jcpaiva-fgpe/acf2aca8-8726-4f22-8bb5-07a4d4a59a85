89d7690255b3f7040ec356a55711796329a211dd
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-24 19:02:50 +0100

    created file exercises/a1858df0-e756-49f7-9ac2-89c1efc6b929/solutions/19eb02bf-78a3-4ce1-ae81-682ca4d656a6/sol.py

---
+++ exercises/a1858df0-e756-49f7-9ac2-89c1efc6b929/solutions/19eb02bf-78a3-4ce1-ae81-682ca4d656a6/sol.py
@@ -0,0 +1,7 @@
+amzius = int(input('Kiek tau metų?\n'))
+
+while amzius < 18:
+    print(f'Iki pilnametystės liko: {18-age} m.')
+    amzius = int(input('Kiek tau metų?\n'))
+
+print('Pagaliau - pilnametis!')
