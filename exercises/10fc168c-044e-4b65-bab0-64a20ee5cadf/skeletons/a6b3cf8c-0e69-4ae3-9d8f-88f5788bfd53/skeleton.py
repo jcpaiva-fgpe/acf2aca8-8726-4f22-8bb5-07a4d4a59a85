b0e1531952b7053b5198cdef001dec16a83ce186
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-24 20:07:59 +0100

    created file exercises/10fc168c-044e-4b65-bab0-64a20ee5cadf/skeletons/a6b3cf8c-0e69-4ae3-9d8f-88f5788bfd53/skeleton.py

---
+++ exercises/10fc168c-044e-4b65-bab0-64a20ee5cadf/skeletons/a6b3cf8c-0e69-4ae3-9d8f-88f5788bfd53/skeleton.py
@@ -0,0 +1,24 @@
+# Pavyzdys:
+def dalinti(a=2, b=3):
+    return a/b
+
+"""
+print(dalinti())
+print(dalinti(1))
+print(dalinti(1, 2))
+print(dalinti(2, 1))
+#"""
+
+
+# Parašykite savo funkciją stars
+def stars():
+    
+	pass
+
+
+# ___________________________________
+# Testuoti stars(). NELIESTI!        #
+# ---------------------------------- #
+try: print(stars(int(input())))      #
+except: print(stars())               #
+# __________________________________ /
