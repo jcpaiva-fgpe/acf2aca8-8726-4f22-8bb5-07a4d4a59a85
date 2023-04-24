f9bacf8f64e781075149c2ed605a5f42feefea6c
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-24 19:19:14 +0100

    created file exercises/55bd116e-0c0e-431d-a95c-b1bb068d114f/solutions/d5a168ba-422f-4108-892b-36136f104651/sol.py

---
+++ exercises/55bd116e-0c0e-431d-a95c-b1bb068d114f/solutions/d5a168ba-422f-4108-892b-36136f104651/sol.py
@@ -0,0 +1,12 @@
+# Greičiausias būdas yra toks, tačiau 
+# reiktų išvedinėti atskiromis eilutėmis, 
+# jei duomenys būtų nežinomi.
+print("""
++--------+--------+
+| Vardas | Amžius |
++--------+--------+
+| Tomas  | 24     |
+| Jonas  | 26     |
+| Justė  | 25     |
++--------+--------+
+""")
