8259d1a3547567281f7f8834d9792131aba40c66
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-24 18:30:49 +0100

    created file exercises/8f3e0008-cf59-4fa9-8ed7-4455b18ea872/skeletons/22d8eda7-7723-469d-a828-a770067e1090/skeleton.py

---
+++ exercises/8f3e0008-cf59-4fa9-8ed7-4455b18ea872/skeletons/22d8eda7-7723-469d-a828-a770067e1090/skeleton.py
@@ -0,0 +1,11 @@
+def qsort(li):
+    #Rašykite kodą čia...
+    pass
+
+
+### Šito kodo nelieskite. ###
+test = list(map(int, input().split()))
+# ===================== #
+print("List:", test)
+print("Sorted:", qsort(test))
+# ===================== #
