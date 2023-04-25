df3128f976fd60ef4abe22cfdcdac8ea62a0d7bf
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-25 03:44:47 +0100

    created file exercises/aa2f4847-7455-40a1-86a8-e7e2a01b8ec7/solutions/41b571b8-32ae-4a26-a075-3c5e222758ac/sol.py

---
+++ exercises/aa2f4847-7455-40a1-86a8-e7e2a01b8ec7/solutions/41b571b8-32ae-4a26-a075-3c5e222758ac/sol.py
@@ -0,0 +1,13 @@
+marke = 'Audi'
+modelis = 'A4'
+metai = 2010
+rida = 124775
+darbinis_turis = 1.6
+ar_yra_technikine = False
+
+print(marke, type(marke))
+print(modelis, type(modelis))
+print(metai, type(metai))
+print(rida, type(rida))
+print(darbinis_turis, type(darbinis_turis))
+print(ar_yra_technikine, type(ar_technikine))
