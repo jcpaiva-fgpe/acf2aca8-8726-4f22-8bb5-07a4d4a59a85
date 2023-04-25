bd9f921506473d4e9bdcc8e9c1224fb652ccc1bd
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-25 03:53:57 +0100

    created file exercises/d6a84489-6560-45bf-befd-4b9d12ecd608/solutions/0275f9fc-f213-407a-8843-003fc396b758/sol.py

---
+++ exercises/d6a84489-6560-45bf-befd-4b9d12ecd608/solutions/0275f9fc-f213-407a-8843-003fc396b758/sol.py
@@ -0,0 +1,7 @@
+print("Įveskite skaičių:")
+x = int(input())
+
+if 10 <=x <=100 and x%3==0: #intervale nuo 10 iki 100 ir dalijasi iš 3.
+    print('Tenkinamos sąlygos.')
+else:
+    print('Visos sąlygos netenkinamos.')
