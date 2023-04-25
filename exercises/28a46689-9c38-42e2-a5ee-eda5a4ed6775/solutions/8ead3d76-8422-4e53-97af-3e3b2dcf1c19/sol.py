0e6fcc1935ffed43dc7050de393560f43ec84c66
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-25 03:53:37 +0100

    created file exercises/28a46689-9c38-42e2-a5ee-eda5a4ed6775/solutions/8ead3d76-8422-4e53-97af-3e3b2dcf1c19/sol.py

---
+++ exercises/28a46689-9c38-42e2-a5ee-eda5a4ed6775/solutions/8ead3d76-8422-4e53-97af-3e3b2dcf1c19/sol.py
@@ -0,0 +1,10 @@
+grade = float(input())
+
+if grade == 10.0:
+    print ('Sveikinimai!')
+elif grade >= 8.0:
+    print ('Gerai!')
+elif grade >= 5.0:
+    print ('Pavyko!')
+else:
+    print ('Kaip gaila...')
