4a10cd1502d60d88682c33b318d7cff85b9a98e6
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-25 03:45:59 +0100

    created file exercises/0beca1d9-e1ca-4567-9927-4bad0f013335/skeletons/d1f48b81-71b0-40bd-a8b2-de279be279b8/skeleton.py

---
+++ exercises/0beca1d9-e1ca-4567-9927-4bad0f013335/skeletons/d1f48b81-71b0-40bd-a8b2-de279be279b8/skeleton.py
@@ -0,0 +1,8 @@
+class Kvadratas: 
+   krastine = 1.0 
+   def perimetras(f):
+       return f.krastine * 4.0
+
+kvadratas = Kvadratas()
+kvadratas.krastine = 3.0 # 7 eilutė
+print(kvadratas.perimetras())
