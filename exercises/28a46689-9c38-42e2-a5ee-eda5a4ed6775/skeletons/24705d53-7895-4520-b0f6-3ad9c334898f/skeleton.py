1d483a91e5b2fe67518f7c558691b8e037f2d24b
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-25 03:53:41 +0100

    created file exercises/28a46689-9c38-42e2-a5ee-eda5a4ed6775/skeletons/24705d53-7895-4520-b0f6-3ad9c334898f/skeleton.py

---
+++ exercises/28a46689-9c38-42e2-a5ee-eda5a4ed6775/skeletons/24705d53-7895-4520-b0f6-3ad9c334898f/skeleton.py
@@ -0,0 +1,10 @@
+grade = float(input())
+
+if grade == 10.0:
+    print ('Sveikinimai!')
+if grade >= 8.0:
+    print ('Gerai!')
+if grade >= 5.0:
+    print ('Pavyko!')
+else:
+    print ('Kaip gaila...')
