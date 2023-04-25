8ff2512bf0b0ebb1325d7c9ca65762c9373540be
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-25 03:54:14 +0100

    created file exercises/2de9b2f4-c966-4d29-af15-2a5f34076fd8/skeletons/183ac139-5035-46f3-86b9-6ddb9af6f157/skeleton.py

---
+++ exercises/2de9b2f4-c966-4d29-af15-2a5f34076fd8/skeletons/183ac139-5035-46f3-86b9-6ddb9af6f157/skeleton.py
@@ -0,0 +1,9 @@
+print('Darbo sutarčių skaičius:') 
+employment_contracts = int(input()) # Integers only
+    
+print('Kompiuteris? True ar False?')
+commuter = input()
+
+costs = 0
+
+print('Metinės pajamų sąnaudos yra', costs, 'eurų.')
