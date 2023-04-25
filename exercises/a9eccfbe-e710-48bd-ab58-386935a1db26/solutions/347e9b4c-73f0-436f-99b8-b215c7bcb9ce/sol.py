ba8fef03aef13f1f56b87acf97e2ba0f727b1825
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-25 03:56:59 +0100

    created file exercises/a9eccfbe-e710-48bd-ab58-386935a1db26/solutions/347e9b4c-73f0-436f-99b8-b215c7bcb9ce/sol.py

---
+++ exercises/a9eccfbe-e710-48bd-ab58-386935a1db26/solutions/347e9b4c-73f0-436f-99b8-b215c7bcb9ce/sol.py
@@ -0,0 +1,8 @@
+darbai = ['skalbti', 'siurbti', 'lyginti', 'virti']
+
+while darbai:
+    darbas = input ('Ką reikia daryti?\n')
+    if darbas not in darbai: continue
+    darbai.remove(job)
+     
+print('Viskas padaryta! Pailsėk!') 
