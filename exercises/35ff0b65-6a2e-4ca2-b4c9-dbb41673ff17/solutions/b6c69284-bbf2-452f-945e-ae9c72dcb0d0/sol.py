5bd437d3576fbf5d6f42cfd01a4ad4ec087ca2f5
Tomas Blazauskas <blaztoma@gmail.com>
2023-04-25 03:46:18 +0100

    created file exercises/35ff0b65-6a2e-4ca2-b4c9-dbb41673ff17/solutions/b6c69284-bbf2-452f-945e-ae9c72dcb0d0/sol.py

---
+++ exercises/35ff0b65-6a2e-4ca2-b4c9-dbb41673ff17/solutions/b6c69284-bbf2-452f-945e-ae9c72dcb0d0/sol.py
@@ -0,0 +1,9 @@
+class Kvadratas: 
+    krastine = 1.0
+    def perimetras(f):
+        return f.krastine * 4.0
+    def __str__(f):
+        return f'Kvadrato kraštinė = {f.krastine}'
+       
+kvadratas = Kvadratas()
+print(kvadratas)
